9c069e23965df6a4cf22834980ab2be51778ffaf
osoriocc <69528464+osoriocc@users.noreply.github.com>
2020-08-21 20:39:10 -0500

    first commit

---
+++ SeleniumAutomation/src/main/java/DataType.java
@@ -0,0 +1,60 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+		byte x;
+		short y;
+		int z;
+		long a;
+
+		x=127;
+        y=32767;
+        z=2147483647;
+		a=9223372036854775807L;
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(a);*/
+		
+//		-------------------
+		/*
+	float base;
+	double altura;
+	base = 30;
+	altura= 40;
+	
+	System.out.println(base);*/
+//*****************************
+		
+		
+	char letra1;
+	char letra2;
+	String frase;
+	
+	letra1= 'a';
+	letra2= '\u0047';
+	frase="Hola";
+	System.out.println(frase);
+	
+	
+	
+
+ /* **********VALORES BOLEANOS****************	
+	boolean avesVuelan;
+	avesVuelan = true;
+	
+	System.out.println("LAS AVES VUELAN?" + avesVuelan);
+	*/
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/OperadoresAritmeticos.java
@@ -0,0 +1,40 @@
+package com;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* OPERADORES ARITMETICOS
+		int x= 1;
+		int y= 2;
+		int suma;
+		
+		suma = x+y;
+		
+		System.out.println(suma);
+		*/
+
+		/* OPERADORES UNARIO*******
+		 int valor1 = 3;
+		int incrementar = ++valor1;
+		System.out.println(incrementar);
+		 */
+		
+		/* OPERADORES RELACIONALES 
+		 *
+		 */
+		
+		int num1=3;
+		int num2=3;
+		
+		if(num1 == num2)
+		{
+			System.out.println("los numeros son iguales");
+		}else 
+			System.out.println("los numeros NO son iguales");
+		
+		
+	}
+
+}
